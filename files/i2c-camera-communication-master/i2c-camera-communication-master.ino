d87ee64962ecfce3df146eaed7ba098cb1b19bca
Maksym <maks.pata@gmail.com>
2024-12-03 18:34:09 +0100

    add master handler code

--- files/i2c-camera-communication-master/i2c-camera-communication-master.ino
+++ files/i2c-camera-communication-master/i2c-camera-communication-master.ino
@@ -1,14 +1,18 @@
 #include <Wire.h>
 
-#define XIAO_ADDR 1
-
 void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-
+  Wire.begin(D1, D2); // D1 = SCL, D2 = SDA (GPIO5, GPIO4)
+  Serial.begin(9600);
 }
 
 void loop() {
-  Wire.beginTransmission(XIAO_ADDR);
+  Wire.beginTransmission(0x42); // Example I2C address
+  Wire.write('1');
+  Wire.endTransmission();
+  delay(1000);
 
+  Wire.beginTransmission(0x42); // Example I2C address
+  Wire.write('0');
+  Wire.endTransmission();
+  delay(1000);
 }
