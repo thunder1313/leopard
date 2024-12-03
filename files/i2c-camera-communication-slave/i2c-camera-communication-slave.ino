c3989966d6a9262b9a62676ee0a6a1cfc41e1752
Maksym <maks.pata@gmail.com>
2024-12-03 18:34:29 +0100

    add slave handler code

--- files/i2c-camera-communication-slave/i2c-camera-communication-slave.ino
+++ files/i2c-camera-communication-slave/i2c-camera-communication-slave.ino
@@ -1,14 +1,29 @@
 #include <Wire.h>
-
-#define XIAO_ADDR 1
+#define XIAO_ADDR 0x42
+#define SDA_PIN 4
+#define SCL_PIN 5
+#define LED_PIN 21
 
 void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-
+  pinMode(LED_PIN, OUTPUT);
+  Wire.begin(SDA_PIN, SCL_PIN, XIAO_ADDR);
+  Wire.onReceive(receiveEvent);
 }
 
 void loop() {
-  Wire.beginTransmission(XIAO_ADDR);
+  // digitalWrite(LED_PIN, LOW);
+  // delay (1000);
+  // digitalWrite(LED_PIN, HIGH);
+  // delay (1000);
+}
 
+void receiveEvent(int bytes) {
+  while (Wire.available()) {
+    char command = Wire.read(); // Read command
+    if (command == '1') {
+      digitalWrite(LED_PIN, HIGH); // Turn LED on
+    } else if (command == '0') {
+      digitalWrite(LED_PIN, LOW);  // Turn LED off
+    }
+  }
 }
